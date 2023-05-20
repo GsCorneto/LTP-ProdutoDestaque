3ccae76730fbe06a75e31cdcef1c08e7fd9db1ba
GsCorneto <131817217+GsCorneto@users.noreply.github.com>
2023-05-19 22:02:59 -0400

    Estilização da pagina

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ class Produtos{
         this.preco = preco
     }
     mostrar_produto(){
-        return "Nome: " + this.nome + " ,Data de Cadastro: " + this.dataCadastro + " ,Descrição: " + this.descricao + " ,Preço: R$" + this.preco
+         return `
+         <div>
+         <h2>${this.nome}</h2>
+            <div>${this.preco}</div>
+            <p>${this.descricao}</p>
+        </div>
+         `
+
+        //return "Nome: " + this.nome + " ,Data de Cadastro: " + this.dataCadastro + " ,Descrição: " + this.descricao + " ,Preço: R$" + this.preco
     }
 }
 const prod = new Produtos("Wesker", "25/04/2023", "Muita Nostalgia", 250)
@@ -14,24 +22,26 @@ console.log(prod.mostrar_produto())
 
 
 class ProdutoDestaque extends Produtos{
-    constructor(nome, dataCadastro, descricao, preco,){
+    constructor(nome, dataCadastro, descricao, preco, img){
         super(nome, dataCadastro, descricao, preco)
+        this.img = img;
     }
     mostrar_produto_destaque(){
           return  `
-            <h1>${this.nome}</h1>
+            <h2>${this.nome}</h2>
             <div>${this.dataCadastro}</div>
             <p>${this.descricao}</p>
-            `
+            <img src= ${this.img} class="imagem"></img>
+                  `
         //return "Nome: " + this.nome + " ,Data de Cadastro: " + this.dataCadastro + " ,Descrição: " + this.descricao + " ,Preço: R$" + this.preco
     }
 }
- const prodDes = new ProdutoDestaque("Leon S. Kennedy", "21/07/1977", "HEEEEELLLPP!", 20)
+ const prodDes = new ProdutoDestaque("Leon S. Kennedy", "21/07/1977", "HEEEEELLLPP!", 20, "https://static.wikia.nocookie.net/residentevil/images/1/12/Image4kq8.jpg/revision/latest?cb=20120414235220&path-prefix=pt-br")
  console.log(prodDes)
 
 let pr1 = document.getElementById("prod")
 let pr2 = document.getElementById("prodDes")
 
-//pr1.insertAdjacentHTML('afterbegin', prod.mostrar_produto())
+pr1.insertAdjacentHTML('afterbegin', prod.mostrar_produto())
 pr2.insertAdjacentHTML('afterbegin', prodDes.mostrar_produto_destaque())
  //divListaProduto.insertAdjacentHTML('afterbegin', '');
